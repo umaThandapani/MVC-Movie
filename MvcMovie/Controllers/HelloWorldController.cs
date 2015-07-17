6e3843e6a773775d9926c7bb9437cc3d4cce2d9b
umathan <umathan13@gmail.com>
2015-07-17 15:46:13 -0400

    Adding View
    In the MVC pattern, view is used and the ViewBag object is retrieved
    from controller to display in the webpage.

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -9,36 +9,50 @@ namespace MvcMovie.Controllers
     public class HelloWorldController : Controller
     {
         //// GET: HelloWorld
-        //public ActionResult Index()
-        //{
-        //    return View();
-        //}
-
-        //// GET: HelloWorld
-        public string index()
+        public ActionResult Index()
         {
-            return "This is my <b> default </b> action... ";
+            return View();
         }
-
-
-        ////// GET: HelloWorld/Welcome
-        //public string Welcome()
-        //{
-        //    return "This is the Welcome action method....";
-        //}
-
-        //// Adding parameters in the welcome method to add query in the browser
-        //public string Welcome(string name, int numTimes = 1)
-        //{
-        //    return HttpUtility.HtmlEncode ("Hello " + name + ", NumTimes is: " + numTimes);
-        //}
-
-      // Changing the parameters
-        public string Welcome(string name, int ID = 1)
+       
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
         }
+        
+    }  
+}      
+        
+        
+// ***************************************
+
+//        //// GET: HelloWorld
+//        //public string index()
+//        //{
+//        //    return "This is my <b> default </b> action... ";
+//        //}
 
 
-    }
-}
+//        ////// GET: HelloWorld/Welcome
+//        //public string Welcome()
+//        //{
+//        //    return "This is the Welcome action method....";
+//        //}
+
+//        //// Adding parameters in the welcome method to add query in the browser
+//        //public string Welcome(string name, int numTimes = 1)
+//        //{
+//        //    return HttpUtility.HtmlEncode ("Hello " + name + ", NumTimes is: " + numTimes);
+//        //}
+
+//      // Changing the parameters
+//        public string Welcome(string name, int ID = 1)
+//        {
+//            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+//        }
+
+
+//    }
+//}
