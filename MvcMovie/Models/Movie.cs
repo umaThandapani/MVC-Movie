65062b10caf3a75fa3832f5380594879c2089329
umathan <umathan13@gmail.com>
2015-07-21 11:39:25 -0400

    Understanding Scaffolding Controllers
    Adding Attributes

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 
 using System.Data.Entity;
@@ -13,6 +14,12 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
+
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
